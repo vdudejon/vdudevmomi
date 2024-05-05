9dfbca698b33d979a89ef36c25f175974b147c6b
vdudejon <jon.somers@gmail.com>
2024-05-05 11:19:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="vdudevmomi",
+    version="0.0.1",
+    url="https://github.com/vdudejon/vdudevmomi",
+    author="vdudejon",
+    packages=find_packages(exclude=["tests", "tests.*", "examples"]),
+    install_requires=[
+        "pyvim>=3.0.3",
+        "pyvmomi>=8.0.2.0",
+        "requests",
+    ],
+)
